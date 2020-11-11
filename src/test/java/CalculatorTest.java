f328e562e45b3b0ebc21f23ac0d9f103344e61fc
Johan Astrom <johan.astrom89@gmail.com>
2020-11-11 20:36:38 +0100

    Added CalcTests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,8 +23,8 @@ public class CalculatorTest{
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 10, -101, Integer.MAX_VALUE-1, Integer.MIN_VALUE})
-    void test_add_method(int num){
+    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE-1, Double.MIN_VALUE})
+    void test_add_method(double num){
         assertEquals(1+num, calc.add(1, num));
     }
 
@@ -33,14 +34,36 @@ public class CalculatorTest{
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 10, -101, Integer.MAX_VALUE, Integer.MIN_VALUE+1})
-    void test_subtract_method(int num){
+    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE, Double.MIN_VALUE+1})
+    void test_subtract_method(double num){
         assertEquals(num-1, calc.subtract(num, 1));
     }
 
     @Test
     void test_subtract_method_not_equals(){
-        assertNotEquals(5, calc.subtract(6, 2));
+        assertNotEquals(5.3, calc.subtract(6.5, 2.2));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1.4, 2.23", "2.123, 3", "10, -101", "2347543.123, 435.2"})
+    void test_multiply_method(double num1, double num2){
+        assertEquals(num1*num2, calc.multiply(num1, num2));
+    }
+
+    @Test
+    void test_multiply_method_not_equals(){
+        assertNotEquals(5.2, calc.multiply(6.23, 2));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1.4, 2.23", "2.123, 3", "10, -101", "2347543.123, 435.2"})
+    void test_divide_method(double num1, double num2){
+        assertEquals(num1*num2, calc.divide(num1, num2));
+    }
+
+    @Test
+    void test_divide_method_not_equals(){
+        assertNotEquals(5.3, calc.multiply(6.234, 2.34));
     }
 
 
