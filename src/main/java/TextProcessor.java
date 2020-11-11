f22eb4d69c84c2cb43028337c9f33779c0f0d639
Johan Astrom <johan.astrom89@gmail.com>
2020-11-11 19:27:14 +0100

    Initial commit

---
+++ src/main/java/TextProcessor.java
@@ -0,0 +1,16 @@
+public class TextProcessor{
+
+public String toUpper(String s){
+return s.toUpperCase();
+}
+
+public String toLower(String s){
+return s.toLowerCase();
+}
+
+public String reverse(String s){
+StringBuilder sb = new StringBuilder();
+return sb.append(s).reverse().toString();
+}
+
+}
