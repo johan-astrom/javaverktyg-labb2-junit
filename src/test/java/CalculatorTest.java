24cfa99b44fbc3d24f1ad2502ead4b12f30f625d
Johan Astrom <johan.astrom89@gmail.com>
2020-11-11 19:54:29 +0100

    Added two CalcTests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,37 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest{
+
+    private Calculator calc;
+
+    @BeforeAll
+    static void message(){
+        System.out.println("Performing Calculator tests...");
+    }
+
+    @BeforeEach
+    void calcInstance(){
+        calc = new Calculator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 10, -101, Integer.MAX_VALUE-1, Integer.MIN_VALUE})
+    void test_add_method(int num){
+        assertEquals(1+num, calc.add(1, num));
+    }
+
+    @Test
+    void test_add_method_not_equals(){
+        assertNotEquals(5, calc.add(2, 2));
+    }
+
 
 
 }
