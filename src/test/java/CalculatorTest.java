e9b863be29e0dee2c68109144611e7a89a66958f
Johan Astrom <johan.astrom89@gmail.com>
2020-11-13 11:26:31 +0100

    Added notnull textProcessor test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,94 +6,94 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest{
+public class CalculatorTest {
 
     private Calculator calc;
 
     @BeforeAll
-    static void message(){
+    static void message() {
         System.out.println("Performing Calculator tests...");
     }
 
     @BeforeEach
-    void calcInstance(){
+    void calcInstance() {
         this.calc = new Calculator();
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE-1, Double.MIN_VALUE})
+    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE - 1, Double.MIN_VALUE})
     @DisplayName("Add method test")
-    void test_add_method(double num){
-        assertEquals(1+num, calc.add(1, num));
+    void test_add_method(double num) {
+        assertEquals(1 + num, calc.add(1, num));
     }
 
     @Test
     @DisplayName("Add method not equals test")
-    void test_add_method_not_equals(){
+    void test_add_method_not_equals() {
         assertNotEquals(5, calc.add(2, 2));
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE, Double.MIN_VALUE+1})
+    @ValueSource(doubles = {1, 2.5, 3.12345, 10.234, -101, -1235.76543, Double.MAX_VALUE, Double.MIN_VALUE + 1})
     @DisplayName("Subtract method test")
-    void test_subtract_method(double num){
-        assertEquals(num-1, calc.subtract(num, 1));
+    void test_subtract_method(double num) {
+        assertEquals(num - 1, calc.subtract(num, 1));
     }
 
     @Test
     @DisplayName("Subtract method not equals test")
-    void test_subtract_method_not_equals(){
+    void test_subtract_method_not_equals() {
         assertNotEquals(5.3, calc.subtract(6.5, 2.2));
     }
 
     @ParameterizedTest
     @CsvSource({"1.4, 2.23", "2.123, 3", "10, -101", "2347543.123, 435.2"})
     @DisplayName("Multiply method test")
-    void test_multiply_method(double num1, double num2){
-        assertEquals(num1*num2, calc.multiply(num1, num2));
+    void test_multiply_method(double num1, double num2) {
+        assertEquals(num1 * num2, calc.multiply(num1, num2));
     }
 
     @Test
     @DisplayName("Multiply method not equals test")
-    void test_multiply_method_not_equals(){
+    void test_multiply_method_not_equals() {
         assertNotEquals(5.2, calc.multiply(6.23, 2));
     }
 
     @ParameterizedTest
     @CsvSource({"1.4, 2.23", "2.123, 3", "10, -101", "2347543.123, 435.2"})
     @DisplayName("Divide method test")
-    void test_divide_method(double num1, double num2){
-        assertEquals(num1/num2, calc.divide(num1, num2));
+    void test_divide_method(double num1, double num2) {
+        assertEquals(num1 / num2, calc.divide(num1, num2));
     }
 
     @Test
     @DisplayName("Divide method not equals test")
-    void test_divide_method_not_equals(){
+    void test_divide_method_not_equals() {
         assertNotEquals(5.3, calc.divide(6.234, 2.34));
     }
 
     @Test
     @DisplayName("Test exception")
-    void test_exception(){
+    void test_exception() {
         assertThrows(NumberFormatException.class, () -> calc.add(Integer.parseInt("a"), 2));
     }
 
     @Disabled
     @DisplayName("Check test speed")
     @RepeatedTest(10000)
-    void repeated_test(){
-        calc.multiply(Integer.MAX_VALUE/2, 2);
+    void repeated_test() {
+        calc.multiply(Integer.MAX_VALUE / 2, 2);
     }
 
     @Disabled
     @Test
     @DisplayName("Cross-method test")
-    void cross_method_test(){
-        assertEquals((3-2)+(2*2), calc.add(calc.subtract(3,2), calc.multiply(2, 2)));
+    void cross_method_test() {
+        assertEquals((3 - 2) + (2 * 2), calc.add(calc.subtract(3, 2), calc.multiply(2, 2)));
     }
 
     @AfterAll
-    static void messageAfter(){
+    static void messageAfter() {
         System.out.println("Calculator tests completed.");
     }
 
--- src/test/java/TextProcessorTest.java
+++ src/test/java/TextProcessorTest.java
@@ -8,70 +8,74 @@ import org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TextProcessorTest{
+public class TextProcessorTest {
 
     private TextProcessor textProcessor;
 
     @BeforeAll
-    static void message(){
+    static void message() {
         System.out.println("Performing TextProcessor tests...");
     }
 
     @BeforeEach
-    void TextProcInstance(){
+    void TextProcInstance() {
         this.textProcessor = new TextProcessor();
     }
 
     @ParameterizedTest
     @NullSource
-    void test_null(String s){
+    void test_null(String s) {
         assertNull(s, "String should be null");
     }
 
     @ParameterizedTest
+    @EmptySource
+    void check_blank_not_equal_to_null(String s) {
+        assertNotNull(textProcessor.toUpper(s));
+    }
+
+    @ParameterizedTest
     @CsvFileSource(resources = "/toUpperValues.csv")
-    void test_toUpper(String input, String result){
+    void test_toUpper(String input, String result) {
         assertEquals(result, textProcessor.toUpper(input));
     }
 
     @ParameterizedTest
     @EmptySource
     @ValueSource(strings = {" ", "\t", "\n"})
-    void test_toUpper_empty(String s){
+    void test_toUpper_empty(String s) {
         assertTrue(textProcessor.toUpper(s).isBlank(), "Should return true for blank strings");
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/toLowerValues.csv")
-    void test_toLower(String input, String result){
+    void test_toLower(String input, String result) {
         assertEquals(result, textProcessor.toLower(input));
     }
 
     @ParameterizedTest
     @EmptySource
     @ValueSource(strings = {" ", "\t", "\n"})
-    void test_toLower_empty(String s){
+    void test_toLower_empty(String s) {
         assertTrue(textProcessor.toLower(s).isBlank(), "Should return true for blank strings");
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/reverseValues.csv")
-    void test_reverse(String input, String result){
+    void test_reverse(String input, String result) {
         assertEquals(result, textProcessor.reverse(input));
     }
 
     @ParameterizedTest
     @EmptySource
     @ValueSource(strings = {" ", "\t", "\n"})
-    void test_reverse_empty(String s){
+    void test_reverse_empty(String s) {
         assertTrue(textProcessor.reverse(s).isBlank(), "Should return true for blank strings");
     }
 
-    
-    
-    
+
     @AfterAll
-    static void messageAfter(){
+    static void messageAfter() {
         System.out.println("TextProcessor tests completed.");
     }
 
