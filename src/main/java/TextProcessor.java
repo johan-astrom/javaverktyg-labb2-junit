99de5880a8230a622b36574b29fa064205f73638
Johan Astrom <johan.astrom89@gmail.com>
2020-12-05 22:13:50 +0100

    Purposely erroneous code to check jenkins e-mail notif. 3 after settings changed in Jenkins

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -12,7 +12,7 @@ public String reverse(String s){
 StringBuilder sb = new StringBuilder();
 return sb.append(s).reverse().toString();
 }
-    erroneous code3
+    erroneous code4 after mail settings update in Jenkins
 
 
 }
